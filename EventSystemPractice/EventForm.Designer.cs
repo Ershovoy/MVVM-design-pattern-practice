f475a97489ccf37a05f123d6d463c62f72296523
Ershovoy <ershov64372@gmail.com>
2022-11-11 10:33:22 +0300

    Create user interface in `EventForm`

--- EventSystemPractice/EventForm.Designer.cs
+++ EventSystemPractice/EventForm.Designer.cs
@@ -28,12 +28,108 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.FullNameLabel = new System.Windows.Forms.Label();
+			this.FullNameTextBox = new System.Windows.Forms.TextBox();
+			this.PhoneNumberLabel = new System.Windows.Forms.Label();
+			this.PhoneNumberTextBox = new System.Windows.Forms.TextBox();
+			this.AddressLabel = new System.Windows.Forms.Label();
+			this.AddressTextBox = new System.Windows.Forms.TextBox();
+			this.CloseButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// FullNameLabel
+			// 
+			this.FullNameLabel.AutoSize = true;
+			this.FullNameLabel.Location = new System.Drawing.Point(12, 24);
+			this.FullNameLabel.Name = "FullNameLabel";
+			this.FullNameLabel.Size = new System.Drawing.Size(64, 15);
+			this.FullNameLabel.TabIndex = 0;
+			this.FullNameLabel.Text = "Full Name:";
+			// 
+			// FullNameTextBox
+			// 
+			this.FullNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.FullNameTextBox.Location = new System.Drawing.Point(12, 42);
+			this.FullNameTextBox.Name = "FullNameTextBox";
+			this.FullNameTextBox.Size = new System.Drawing.Size(200, 23);
+			this.FullNameTextBox.TabIndex = 1;
+			// 
+			// PhoneNumberLabel
+			// 
+			this.PhoneNumberLabel.AutoSize = true;
+			this.PhoneNumberLabel.Location = new System.Drawing.Point(12, 83);
+			this.PhoneNumberLabel.Name = "PhoneNumberLabel";
+			this.PhoneNumberLabel.Size = new System.Drawing.Size(91, 15);
+			this.PhoneNumberLabel.TabIndex = 2;
+			this.PhoneNumberLabel.Text = "Phone Number:";
+			// 
+			// PhoneNumberTextBox
+			// 
+			this.PhoneNumberTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.PhoneNumberTextBox.Location = new System.Drawing.Point(12, 101);
+			this.PhoneNumberTextBox.Name = "PhoneNumberTextBox";
+			this.PhoneNumberTextBox.Size = new System.Drawing.Size(200, 23);
+			this.PhoneNumberTextBox.TabIndex = 3;
+			// 
+			// AddressLabel
+			// 
+			this.AddressLabel.AutoSize = true;
+			this.AddressLabel.Location = new System.Drawing.Point(12, 142);
+			this.AddressLabel.Name = "AddressLabel";
+			this.AddressLabel.Size = new System.Drawing.Size(49, 15);
+			this.AddressLabel.TabIndex = 4;
+			this.AddressLabel.Text = "Address";
+			// 
+			// AddressTextBox
+			// 
+			this.AddressTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.AddressTextBox.Location = new System.Drawing.Point(12, 160);
+			this.AddressTextBox.Name = "AddressTextBox";
+			this.AddressTextBox.Size = new System.Drawing.Size(200, 23);
+			this.AddressTextBox.TabIndex = 5;
+			// 
+			// CloseButton
+			// 
+			this.CloseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.CloseButton.Location = new System.Drawing.Point(137, 246);
+			this.CloseButton.Name = "CloseButton";
+			this.CloseButton.Size = new System.Drawing.Size(75, 23);
+			this.CloseButton.TabIndex = 6;
+			this.CloseButton.Text = "Close";
+			this.CloseButton.UseVisualStyleBackColor = true;
+			// 
+			// EventForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(224, 281);
+			this.Controls.Add(this.CloseButton);
+			this.Controls.Add(this.AddressTextBox);
+			this.Controls.Add(this.AddressLabel);
+			this.Controls.Add(this.PhoneNumberTextBox);
+			this.Controls.Add(this.PhoneNumberLabel);
+			this.Controls.Add(this.FullNameTextBox);
+			this.Controls.Add(this.FullNameLabel);
+			this.MinimumSize = new System.Drawing.Size(240, 320);
+			this.Name = "EventForm";
+			this.ShowIcon = false;
+			this.Text = "Main Form";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private Label FullNameLabel;
+		private TextBox FullNameTextBox;
+		private Label PhoneNumberLabel;
+		private TextBox PhoneNumberTextBox;
+		private Label AddressLabel;
+		private TextBox AddressTextBox;
+		private Button CloseButton;
 	}
 }
