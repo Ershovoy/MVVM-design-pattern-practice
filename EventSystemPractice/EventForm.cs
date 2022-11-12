0d0a55ee8c48662de83fa0606284a0ebcdf2cb99
Ershovoy <ershov64372@gmail.com>
2022-11-12 13:24:49 +0300

    Implement `EventForm` functionality

--- EventSystemPractice/EventForm.cs
+++ EventSystemPractice/EventForm.cs
@@ -1,10 +1,77 @@
-namespace EventSystemPractice
+namespace EventSystemPractice;
+
+public partial class EventForm : Form
 {
-	public partial class EventForm : Form
+	private Contact _contact;
+
+	public Contact Contact
 	{
-		public EventForm()
+		get { return _contact; }
+		set
 		{
-			InitializeComponent();
+			_contact = value;
+			_contact.FullNameChanged    += FullNameChanged;
+			_contact.PhoneNumberChanged += PhoneNumberChanged;
+			_contact.AddressChanged     += AddressChanged;
 		}
+	}
+
+	public EventForm(Contact bindedContact, string formTitle)
+	{
+		InitializeComponent();
+
+		Contact = bindedContact;
+		Text = formTitle;
+	}
+
+	public void FullNameChanged(object? sender, EventArgs e)
+	{
+		if (sender is Contact contact)
+		{
+			FullNameTextBox.Text = contact.FullName;
+		}
+	}
+
+	public void PhoneNumberChanged(object? sender, EventArgs e)
+	{
+		if (sender is Contact contact)
+		{
+			PhoneNumberTextBox.Text = contact.PhoneNumber;
+		}
+	}
+
+	public void AddressChanged(object? sender, EventArgs e)
+	{
+		if (sender is Contact contact)
+		{
+			AddressTextBox.Text = contact.Address;
+		}
+	}
+
+	private void FullNameTextBox_TextChanged(object sender, EventArgs e)
+	{
+		Contact.FullName = FullNameTextBox.Text;
+	}
+
+	private void PhoneNumberTextBox_TextChanged(object sender, EventArgs e)
+	{
+		Contact.PhoneNumber = PhoneNumberTextBox.Text;
+	}
+
+	private void AddressTextBox_TextChanged(object sender, EventArgs e)
+	{
+		Contact.Address = AddressTextBox.Text;
+	}
+
+	private void EventForm_FormClosing(object sender, FormClosingEventArgs e)
+	{
+		Contact.FullNameChanged    -= FullNameChanged;
+		Contact.PhoneNumberChanged -= PhoneNumberChanged;
+		Contact.AddressChanged     -= AddressChanged;
+	}
+
+	private void CloseButton_Click(object sender, EventArgs e)
+	{
+		Close();
 	}
 }
