9ee915a19d1612ac5b2568bd7b1fc7090b4c52d5
Ershovoy <ershov64372@gmail.com>
2022-11-13 12:31:12 +0300

    Create two additional `EventForm` to demonstrate event system work

--- EventSystemPractice/Program.cs
+++ EventSystemPractice/Program.cs
@@ -1,17 +1,25 @@
-namespace EventSystemPractice
+namespace EventSystemPractice;
+
+internal static class Program
 {
-	internal static class Program
+	/// <summary>
+	///  The main entry point for the application.
+	/// </summary>
+	[STAThread]
+	static void Main()
 	{
-		/// <summary>
-		///  The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main()
+		// To customize application configuration such as set high DPI settings or default font,
+		// see https://aka.ms/applicationconfiguration.
+		ApplicationConfiguration.Initialize();
+
+		Contact contact = new();
+		for(int i = 0; i < 2; i++)
 		{
-			// To customize application configuration such as set high DPI settings or default font,
-			// see https://aka.ms/applicationconfiguration.
-			ApplicationConfiguration.Initialize();
-			Application.Run(new EventForm());
+			EventForm secondaryForm = new(contact, "Secondary Form");
+			secondaryForm.Show();
 		}
+
+		EventForm mainForm = new(contact, "Main Form");
+		Application.Run(mainForm);
 	}
 }
